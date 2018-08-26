f9aee53db74aa50cff2677a56f4cf8e90e2551f6
renato.vicente <rntvicente@gmail.com>
2018-08-26 17:19:12 -0300

    Adicionando rotas e template storystrap

--- app.js
+++ app.js
@@ -6,20 +6,35 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
+
+bookRouter.route('/')
+  .get((req, res) => {
+    res.status('200').send('Hello Books');
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.status('200').send('Hello Single Book');
+  });
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/books', bookRouter);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/src/views'));
 
 app.get('/', (req, res) => {
   res.render('index', {
-    title: 'Título',
-    list: ['a', 'b', 'c']
+    title: 'Library',
+    nav: [
+      { link: '/books', title: 'Books' },
+      { link: '/authors', title: 'Authors' }
+    ]
   });
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
